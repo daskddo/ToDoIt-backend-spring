7c01bbad3323f189962d1f6a183bc52cf24f9fea
kim <daskddo@naver.com>
2024-06-26 15:16:09 +0900

    :recycle: 코드 최적화

--- src/main/java/ToDoIt/backend/repository/UserRepository.java
+++ src/main/java/ToDoIt/backend/repository/UserRepository.java
@@ -9,10 +9,7 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     Users findByEmail(String email);
     Users findByUserID(String userID);
 
-    Users findByEmailAndPassword(String email, String password);
-
     Users findByPhoneAndUserID(String phone, String userID);
 
     Users findByEmailAndPhoneAndUserID(String email, String phone, String userID);
-
 }
