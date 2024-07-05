d7b5dd17566a88896236683328c4dc38462731f6
kim <daskddo@naver.com>
2024-07-05 17:20:36 +0900

    :recycle: 코드 최적화

--- src/main/java/ToDoIt/backend/domain/ToDo.java
+++ src/main/java/ToDoIt/backend/domain/ToDo.java
@@ -2,7 +2,6 @@ package ToDoIt.backend.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
--- src/main/java/ToDoIt/backend/security/SecurityConfig.java
+++ src/main/java/ToDoIt/backend/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package ToDoIt.backend.security;
 import ToDoIt.backend.jwt.JwtTokenFilter;
 import ToDoIt.backend.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
