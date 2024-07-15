e49fae0ec5dc4e17df5e623933114e9dd3b7652f
kim <daskddo@naver.com>
2024-07-15 15:10:43 +0900

    :recycle: 코드 최적화

--- src/main/java/ToDoIt/backend/repository/ToDoRepository.java
+++ src/main/java/ToDoIt/backend/repository/ToDoRepository.java
@@ -4,13 +4,11 @@ import ToDoIt.backend.domain.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
-    List<ToDo> findByUserEmailAndTaskDate(String email, LocalDate dueDate);
     List<ToDo> findAllByUserEmail(String email);
     List<ToDo> findByUserEmailAndTaskDateIsNull(String email);
     Optional<ToDo> findByUuidAndUserEmail(String uuid, String email);
